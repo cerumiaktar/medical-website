05329065e4905e6b52f223cbf4eae6dc17af4460
unknown <ce.rumiakter@gmail.com>
2025-09-29 16:24:26 +0600

    update hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
                 {/* <div className="hero-overlay bg-opacity-60"></div> */}
                 <div className="hero-content text-neutral-content text-center">
                     <div className="">
-                        <h1 className="mb-5 text-3xl md:text-5xl mx-auto md:w-7/12  font-bold">Your Health, Our Priority Trusted Care for Every Patient</h1>
-                        <p className="mb-5 md:w-6/12 mx-auto">
+                        <h1 className="mb-5 text-3xl md:text-5xl mx-auto md:w-7/12 dark:text-white font-bold">Your Health, Our Priority Trusted Care for Every Patient</h1>
+                        <p className="mb-5 md:w-6/12 dark:text-[#EAEAEA] mx-auto">
                         We are committed to providing trusted, compassionate care for every patient, ensuring that you receive the attention and expertise you deserve for a healthier, happier life.
                         </p>
-                        <button className="btn bg-white text-lg">Get Started</button>
+                        <button className="btn bg-white dark:border-none dark:text-black dark:hover:text-white text-lg">Get Started</button>
                     </div>
                 </div>
             </div>
