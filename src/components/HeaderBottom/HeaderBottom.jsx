aff78c3b132f2341f7677bc0dda38506f076dc6b
unknown <ce.rumiakter@gmail.com>
2025-09-29 16:34:24 +0600

    update herobottom section

--- src/components/HeaderBottom/HeaderBottom.jsx
+++ src/components/HeaderBottom/HeaderBottom.jsx
@@ -5,21 +5,33 @@ import { FaPersonPregnant } from "react-icons/fa6";
 const HeaderBottom = () => {
     return (
         <div className="mt-12">
-            <div className="flex flex-col md:flex-row justify-between">
-                <div className="bg-white space-y-2 rounded-lg shadow-lg py-6 border text-center">
-                    <p className="text-5xl flex justify-center"><FaAmbulance /></p>
-                    <h1 className="text-xl font-medium">24/7 Emergency Services</h1>
-                    <p className="w-8/12 mx-auto text-[#494949]">Immediate and life-saving medical attention with a dedicated <br /> emergency.</p>
+            <div className="flex flex-col md:flex-row gap-8 justify-between">
+                <div className="card bg-base-100 dark:bg-white dark:text-black shadow-2xl">
+                    <figure className="px-10 pt-10 text-5xl">
+                        <FaAmbulance />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">24/7 Emergency Services</h2>
+                        <p>Immediate and life-saving medical attention with a dedicated emergency</p>
+                    </div>
                 </div>
-                <div className="bg-white space-y-2 rounded-lg shadow-lg py-6 border text-center">
-                    <p className="text-5xl flex justify-center"><LuHospital /></p>
-                    <h1 className="text-xl font-medium">Advanced Diagnostic Center</h1>
-                    <p className="w-8/12 mx-auto text-[#494949]">Immediate and life-saving medical attention with a dedicated <br /> emergency.</p>
+                <div className="card bg-base-100 dark:bg-white dark:text-black shadow-2xl">
+                    <figure className="px-10 pt-10 text-5xl">
+                        <LuHospital />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Advanced Diagnostic Center</h2>
+                        <p>Immediate and life-saving medical attention with a dedicated emergency</p>
+                    </div>
                 </div>
-                <div className="bg-white space-y-2 rounded-lg shadow-lg py-6 border text-center">
-                    <p className="text-5xl flex justify-center"><FaPersonPregnant /></p>
-                    <h1 className="text-xl font-medium">Maternity and Neonatal Care</h1>
-                    <p className="w-8/12 mx-auto text-[#494949]">Immediate and life-saving medical attention with a dedicated <br /> emergency.</p>
+                <div className="card bg-base-100 dark:bg-white dark:text-black shadow-2xl">
+                    <figure className="px-10 pt-10 text-5xl">
+                        <FaPersonPregnant />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Maternity and Neonatal Care</h2>
+                        <p>Immediate and life-saving medical attention with a dedicated emergency</p>
+                    </div>
                 </div>
             </div>
         </div>
