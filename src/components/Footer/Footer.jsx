776c3a7f35e4042243252ece34e98476decf0439
unknown <ce.rumiakter@gmail.com>
2025-09-29 17:28:38 +0600

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaFacebook,FaLinkedin, FaYoutube  } from "react-icons/fa";
 
 
 const Footer = () => {
@@ -5,28 +6,28 @@ const Footer = () => {
         <div className="bg-[#46C9BA]">
             <footer className="footer container mx-auto text-base-content p-10">
                 <nav>
-                    <h6 className="text-2xl font-medium">About</h6>
-                    <a className="link link-hover">Home</a>
-                    <a className="link link-hover">Blog</a>
-                    <a className="link link-hover">FAQ</a>
+                    <h6 className="text-2xl font-medium dark:text-black">About</h6>
+                    <a className="link link-hover dark:text-[#333333]">Home</a>
+                    <a className="link link-hover dark:text-[#333333]">Blog</a>
+                    <a className="link link-hover dark:text-[#333333]">FAQ</a>
                 </nav>
                 <nav>
-                    <h6 className="text-2xl font-medium">Contact</h6>
-                    <a className="link link-hover">Address: Dhaka, Bangladesh</a>
-                    <a className="link link-hover">Phone: ++0131234567891</a>
-                    <a className="link link-hover">Email: info@gmail.com</a>
+                    <h6 className="text-2xl font-medium dark:text-black">Contact</h6>
+                    <a className="link link-hover dark:text-[#333333]">Address: Dhaka, Bangladesh</a>
+                    <a className="link link-hover dark:text-[#333333]">Phone: ++0131234567891</a>
+                    <a className="link link-hover dark:text-[#333333]">Email: info@gmail.com</a>
                 </nav>
                 <nav>
-                    <h6 className="text-2xl font-medium">Useful Link</h6>
-                    <a className="link link-hover">Terms & Condition</a>
-                    <a className="link link-hover">Privacy Policy</a>
-                    <a className="link link-hover">Support</a>
+                    <h6 className="text-2xl font-medium dark:text-black">Useful Link</h6>
+                    <a className="link link-hover dark:text-[#333333]">Terms & Condition</a>
+                    <a className="link link-hover dark:text-[#333333]">Privacy Policy</a>
+                    <a className="link link-hover dark:text-[#333333]">Support</a>
                 </nav>
                 <nav>
-                    <h6 className="text-2xl font-medium">Follow Us</h6>
-                    <a className="link link-hover">Facebook</a>
-                    <a className="link link-hover">Linkedin</a>
-                    <a className="link link-hover">Youtube</a>
+                    <h6 className="text-2xl font-medium dark:text-black">Follow Us</h6>
+                    <a className="link link-hover dark:text-[#333333] flex items-center gap-2"><FaFacebook />Facebook</a>
+                    <a className="link link-hover dark:text-[#333333] flex items-center gap-2"><FaLinkedin />Linkedin</a>
+                    <a className="link link-hover dark:text-[#333333] flex items-center gap-2"><FaYoutube />Youtube</a>
                 </nav>
             </footer>
         </div>
