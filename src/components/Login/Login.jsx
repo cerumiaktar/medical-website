b7e6959df5d94eef06176be9471cb025bda200b9
unknown <ce.rumiakter@gmail.com>
2025-10-01 16:21:58 +0600

    update login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,30 +4,30 @@ import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen">
+            <div className="hero bg-base-200 dark:bg-white min-h-screen">
                 <div className="hero-content flex-col">
                     <div className="text-center ">
                         <h1 className="text-5xl font-bold">Login now!</h1>
                     </div>
-                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card bg-base-100 dark:bg-white w-full max-w-sm shrink-0 shadow-2xl">
                         <form className="card-body">
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text dark:text-black">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" placeholder="email" className="input input-bordered dark:bg-white" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text dark:text-black">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" placeholder="password" className="input input-bordered dark:bg-white" required />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <a href="#" className="label-text-alt link link-hover dark:text-black">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn bg-[#46C9BA] dark:text-black dark:border-none">Login</button>
                             </div>
                             <p>Do not have account? <Link to='/signUp' className="text-blue-600 font-medium">SignUp</Link> </p>
                         </form>
