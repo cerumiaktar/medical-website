6546049e9764eb4ac20ecfd3bf95783ff3c0ce20
unknown <ce.rumiakter@gmail.com>
2025-10-01 16:15:46 +0600

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,14 +3,14 @@
 const Contact = () => {
     return (
         <div className="container mx-auto flex justify-center mt-32">
-            <div className="card bg-[#46C9BA] border border-[#46C9BA] w-96">
+            <div className="card bg-[#46C9BA] dark:text-black border border-[#46C9BA] w-96">
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">Contact</h2>
                     <p>Address:Dhaka, Bangladesh</p>
                     <p>Phone: ++8801323456789</p>
                     <p>Email:info@medical.com</p>
                     <div className="card-actions">
-                        <button className="btn bg-white border-none">Contact Us</button>
+                        <button className="btn bg-white border-none dark:text-black">Contact Us</button>
                     </div>
                 </div>
             </div>
