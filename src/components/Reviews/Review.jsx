854263645939b55686878d5e00fc536322e6fda2
unknown <ce.rumiakter@gmail.com>
2025-10-25 11:03:04 +0600

    update

--- src/components/Reviews/Review.jsx
+++ src/components/Reviews/Review.jsx
@@ -3,11 +3,16 @@ const Review = ({ review }) => {
     const { profile, name, designation, review_description } = review;
     return (
         <div className="mx-6">
-            <div className="card card-border bg-base-100 border w-full">
+            <div className="card card-border bg-base-100 border w-96">
                 <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-
+                    <div className="flex items-center gap-4">
+                        <img src={profile} alt="" />
+                        <div>
+                            <h2 className="card-title">{name}</h2>
+                            <p>{designation}</p>
+                        </div>
+                    </div>
+                    <p>{review_description}</p>
                 </div>
             </div>
         </div>
