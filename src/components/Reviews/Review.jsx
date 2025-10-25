8e6fe2deb0e9f4e142c4833cc149fe1f447e3c3f
unknown <ce.rumiakter@gmail.com>
2025-10-25 11:57:02 +0600

    update

--- src/components/Reviews/Review.jsx
+++ src/components/Reviews/Review.jsx
@@ -3,16 +3,16 @@ const Review = ({ review }) => {
     const { profile, name, designation, review_description } = review;
     return (
         <div className="mx-6">
-            <div className="card card-border bg-[#F1F3FF]  border w-96">
+            <div className="card card-border bg-[#F1F3FF] border w-96">
                 <div className="card-body">
                     <div className="flex items-center gap-4">
                         <img src={profile} alt="" />
                         <div>
-                            <h2 className="card-title">{name}</h2>
-                            <p>{designation}</p>
+                            <h2 className="card-title dark:text-black">{name}</h2>
+                            <p className="dark:text-[#333333]">{designation}</p>
                         </div>
                     </div>
-                    <p>{review_description}</p>
+                    <p className="dark:text-[#333333]">{review_description}</p>
                 </div>
             </div>
         </div>
