ce1fef84435d7c4a1332776fdf2f909ba88f3562
unknown <ce.rumiakter@gmail.com>
2025-10-26 11:39:08 +0600

    update

--- src/components/Doctors/Doctors.jsx
+++ src/components/Doctors/Doctors.jsx
@@ -17,33 +17,33 @@ const Doctors = () => {
                 <div className="hero-content text-center">
                     <div className="max-w-3xl">
                         <h1 className="text-5xl mb-8 font-bold text-white">Doctors</h1>
-                        <div className="bg-white py-6 px-6 rounded-lg grid grid-cols-1 md:grid-cols-4 gap-6">
+                        <div className="bg-white py-6 px-6 rounded-lg grid grid-cols-1 md:grid-cols-4 items-center gap-6">
                             {/* Doctor Dropdown */}
                             <div className="dropdown dropdown-start w-full">
-                                <div tabIndex={0} role="button" className="btn m-1">Name <span><MdArrowDropDown /></span></div>
+                                <div tabIndex={0} role="button" className="btn m-1 w-full">Doctors Name <span><MdArrowDropDown /></span></div>
                                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                                    <li><a>Item 1</a></li>
-                                    <li><a>Item 2</a></li>
+                                    <li><a>Dr. Noah Smith</a></li>
+                                    <li><a>Dr. Mia Davis</a></li>
                                 </ul>
                             </div>
                             {/* Department Dropdown */}
                             <div className="dropdown dropdown-start w-full">
-                                <div tabIndex={0} role="button" className="btn m-1">Department <span><MdArrowDropDown /></span></div>
+                                <div tabIndex={0} role="button" className="btn m-1 w-full">Department <span><MdArrowDropDown /></span></div>
                                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                                    <li><a>Item 1</a></li>
-                                    <li><a>Item 2</a></li>
+                                    <li><a>Orthopedic Surgeon</a></li>
+                                    <li><a>ENT Specialist</a></li>
                                 </ul>
                             </div>
                             {/* Hospital Dropdown */}
                             <div className="dropdown dropdown-start w-full">
-                                <div tabIndex={0} role="button" className="btn m-1">Hospital <span><MdArrowDropDown /></span></div>
+                                <div tabIndex={0} role="button" className="btn m-1 w-full">Hospital <span><MdArrowDropDown /></span></div>
                                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                                    <li><a>Item 1</a></li>
-                                    <li><a>Item 2</a></li>
+                                    <li><a>Toronto General Hospital</a></li>
+                                    <li><a>Sheba Medical Center</a></li>
                                 </ul>
                             </div>
                             <div className="w-full">
-                                <button className="btn"><MdOutlineSearch />Like</button>
+                                <button className="btn bg-[#0341C9] w-full text-white hover:bg-[#38BDF8]"><MdOutlineSearch />Search</button>
                             </div>
                         </div>
                     </div>
