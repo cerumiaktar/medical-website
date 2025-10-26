afb1329b72aa7a12c338a853cc7cb1bfeaafd734
unknown <ce.rumiakter@gmail.com>
2025-10-26 11:03:55 +0600

    update doctor page

--- src/components/Doctors/Doctors.jsx
+++ src/components/Doctors/Doctors.jsx
@@ -5,17 +5,24 @@ import Doctor from "../Doctor/Doctor";
 const Doctors = () => {
     const [allDoctors, setAllDoctors] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('doctors.json')
-        .then(res =>res.json())
-        .then(data =>setAllDoctors(data))
-    } ,[])
+            .then(res => res.json())
+            .then(data => setAllDoctors(data))
+    }, [])
     return (
         <div className="container mx-auto mt-8 mb-12">
-            <h1 className="text-2xl font-medium text-center dark:text-black">Doctors</h1>
+            <div className="hero bg-[#0341C9] h-[300px]">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold text-white">Doctors</h1>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
                 {
-                    allDoctors.map(doctor =><Doctor doctor={doctor}></Doctor>)
+                    allDoctors.map(doctor => <Doctor doctor={doctor}></Doctor>)
                 }
             </div>
         </div>
