d9a527d4f1713abccfa6f8ee20870625392b120b
unknown <ce.rumiakter@gmail.com>
2025-10-26 11:21:52 +0600

    update

--- src/components/Doctors/Doctors.jsx
+++ src/components/Doctors/Doctors.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Doctor from "../Doctor/Doctor";
-import { MdArrowDropDown } from "react-icons/md";
+import { MdArrowDropDown, MdOutlineSearch } from "react-icons/md";
 
 
 const Doctors = () => {
@@ -15,11 +15,11 @@ const Doctors = () => {
         <div className="container mx-auto mt-8 mb-12">
             <div className="hero bg-[#0341C9] h-[300px] rounded-xl">
                 <div className="hero-content text-center">
-                    <div className="max-w-md">
-                        <h1 className="text-5xl font-bold text-white">Doctors</h1>
-                        <div className="bg-white py-6 rounded-lg">
+                    <div className="max-w-3xl">
+                        <h1 className="text-5xl mb-8 font-bold text-white">Doctors</h1>
+                        <div className="bg-white py-6 px-6 rounded-lg grid grid-cols-1 md:grid-cols-4 gap-6">
                             {/* Doctor Dropdown */}
-                            <div className="dropdown dropdown-start">
+                            <div className="dropdown dropdown-start w-full">
                                 <div tabIndex={0} role="button" className="btn m-1">Name <span><MdArrowDropDown /></span></div>
                                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                                     <li><a>Item 1</a></li>
@@ -27,7 +27,7 @@ const Doctors = () => {
                                 </ul>
                             </div>
                             {/* Department Dropdown */}
-                            <div className="dropdown dropdown-start">
+                            <div className="dropdown dropdown-start w-full">
                                 <div tabIndex={0} role="button" className="btn m-1">Department <span><MdArrowDropDown /></span></div>
                                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                                     <li><a>Item 1</a></li>
@@ -35,12 +35,15 @@ const Doctors = () => {
                                 </ul>
                             </div>
                             {/* Hospital Dropdown */}
-                            <div className="dropdown dropdown-start">
+                            <div className="dropdown dropdown-start w-full">
                                 <div tabIndex={0} role="button" className="btn m-1">Hospital <span><MdArrowDropDown /></span></div>
                                 <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                                     <li><a>Item 1</a></li>
                                     <li><a>Item 2</a></li>
                                 </ul>
+                            </div>
+                            <div className="w-full">
+                                <button className="btn"><MdOutlineSearch />Like</button>
                             </div>
                         </div>
                     </div>
