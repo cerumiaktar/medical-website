d14ac49578659a09377f778fc2561dd978c14131
unknown <ce.rumiakter@gmail.com>
2025-10-29 16:57:49 +0600

    update

--- src/components/Featureds/Featureds.jsx
+++ src/components/Featureds/Featureds.jsx
@@ -14,24 +14,24 @@ const Featureds = () => {
                 </div>
             </div>
             <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
-                <div className="card-body text-center">
-                    <span className="text-5xl dark:text-black mx-auto"><FaUserDoctor /></span>
-                    <h2 className="card-title dark:text-black mx-auto">Licensed Doctors</h2>
-                    <p className="dark:text-[#333333]">Connect with certified doctors providing trusted advice and care daily.</p>
+                <div className="card-body text-center dark:text-black dark:hover:text-white">
+                    <span className="text-5xl  mx-auto"><FaUserDoctor /></span>
+                    <h2 className="card-title mx-auto">Licensed Doctors</h2>
+                    <p className="">Connect with certified doctors providing trusted advice and care daily.</p>
                 </div>
             </div>
             <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
-                <div className="card-body text-center">
-                    <span className="text-5xl dark:text-black mx-auto"><SiGooglemeet /></span>
-                    <h2 className="card-title dark:text-black mx-auto">Online Consultation</h2>
-                    <p className="dark:text-[#333333]">Reliable and speedy delivery of all health products to you.</p>
+                <div className="card-body text-center dark:text-black dark:hover:text-white">
+                    <span className="text-5xl  mx-auto"><SiGooglemeet /></span>
+                    <h2 className="card-title  mx-auto">Online Consultation</h2>
+                    <p className="">Reliable and speedy delivery of all health products to you.</p>
                 </div>
             </div>
             <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
-                <div className="card-body text-center">
-                    <span className="text-5xl dark:text-black mx-auto"><BsDatabaseFillLock /></span>
-                    <h2 className="card-title dark:text-black mx-auto">Secure Data</h2>
-                    <p className="dark:text-[#333333]">Your personal health information is fully protected and private.</p>
+                <div className="card-body text-center dark:text-black dark:hover:text-white">
+                    <span className="text-5xl  mx-auto"><BsDatabaseFillLock /></span>
+                    <h2 className="card-title  mx-auto">Secure Data</h2>
+                    <p className="">Your personal health information is fully protected and private.</p>
                 </div>
             </div>
         </div>
