697b9e91d8b4b95b7f77ad919c96ce51666b5cff
unknown <ce.rumiakter@gmail.com>
2025-10-29 16:43:51 +0600

    update

--- src/components/Featureds/Featureds.jsx
+++ src/components/Featureds/Featureds.jsx
@@ -13,21 +13,21 @@ const Featureds = () => {
                     <p className="dark:text-[#333333]">High-quality, lab-tested products for safe and reliable health.</p>
                 </div>
             </div>
-            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] ">
+            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
                 <div className="card-body text-center">
                     <span className="text-5xl dark:text-black mx-auto"><FaUserDoctor /></span>
                     <h2 className="card-title dark:text-black mx-auto">Licensed Doctors</h2>
                     <p className="dark:text-[#333333]">Connect with certified doctors providing trusted advice and care daily.</p>
                 </div>
             </div>
-            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] ">
+            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
                 <div className="card-body text-center">
                     <span className="text-5xl dark:text-black mx-auto"><SiGooglemeet /></span>
                     <h2 className="card-title dark:text-black mx-auto">Online Consultation</h2>
                     <p className="dark:text-[#333333]">Reliable and speedy delivery of all health products to you.</p>
                 </div>
             </div>
-            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8]">
+            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
                 <div className="card-body text-center">
                     <span className="text-5xl dark:text-black mx-auto"><BsDatabaseFillLock /></span>
                     <h2 className="card-title dark:text-black mx-auto">Secure Data</h2>
