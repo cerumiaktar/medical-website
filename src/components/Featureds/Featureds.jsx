f94700f7effe8de996bc4134a5c36308b0fc806d
unknown <ce.rumiakter@gmail.com>
2025-10-29 16:55:55 +0600

    update

--- src/components/Featureds/Featureds.jsx
+++ src/components/Featureds/Featureds.jsx
@@ -6,11 +6,11 @@ import { BsDatabaseFillLock } from "react-icons/bs";
 const Featureds = () => {
     return (
         <div className="mt-12 grid grid-cols-2 md:grid-cols-4 gap-6">
-            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
-                <div className="card-body text-center">
-                    <span className="text-5xl dark:text-black mx-auto"><BiSupport /></span>
-                    <h2 className="card-title dark:text-black mx-auto">24/7 Services</h2>
-                    <p className="dark:text-[#333333]">High-quality, lab-tested products for safe and reliable health.</p>
+            <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:dark:text-white hover:-translate-y-3 duration-700 ease-in-out">
+                <div className="card-body text-center dark:text-black dark:hover:text-white">
+                    <span className="text-5xl  dark:hover:text-white mx-auto"><BiSupport /></span>
+                    <h2 className="card-title mx-auto">24/7 Services</h2>
+                    <p className="">High-quality, lab-tested products for safe and reliable health.</p>
                 </div>
             </div>
             <div className="card card-border bg-base-100 dark:bg-white border border-[#b3cbf8] hover:bg-[#0341C9] hover:text-white hover:-translate-y-3 duration-700 ease-in-out">
