11c46bbc3e2a93f08c810aab72fb8b7b0d1a3e51
unknown <ce.rumiakter@gmail.com>
2025-11-01 16:28:31 +0600

    update

--- src/components/AllServices/AllServices.jsx
+++ src/components/AllServices/AllServices.jsx
@@ -15,7 +15,7 @@ const AllServices = () => {
             <div className="hero bg-[#0341C9] h-[300px] rounded-xl mb-6">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
-                        <h1 className="text-5xl font-bold text-white">All Services</h1>
+                        <h1 className="text-5xl font-bold text-white">Our Services</h1>
                     </div>
                 </div>
             </div>
