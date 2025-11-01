e93b1de75ce21217799cdd4e39e93b3adf511447
unknown <ce.rumiakter@gmail.com>
2025-11-01 11:31:40 +0600

    update doctor json file

--- src/components/BestDoctor/BestDoctor.jsx
+++ src/components/BestDoctor/BestDoctor.jsx
@@ -1,7 +1,7 @@
 import { FaFacebookF, FaLinkedinIn, FaInstagram,FaRegUser } from "react-icons/fa6";
 
 const BestDoctor = ({ doctor }) => {
-    const {img,name,designation} = doctor;
+    const {img,name,department} = doctor;
     return (
         <div>
             <div className="card card-compact bg-base-100 dark:bg-white dark:text-black shadow-xl border">
@@ -12,8 +12,8 @@ const BestDoctor = ({ doctor }) => {
                 </figure>
                 <div className="card-body text-center">
                     <h2 className="card-title font-medium mx-auto">{name}</h2>
-                    <p className="text-base font-medium">{designation}</p>
-                    <div>
+                    <p className="text-base font-medium">{department}</p>
+                    <div className="flex items-center gap-3">
                         <button className="btn w-full text-white bg-[#0341C9] dark:border-none hover:bg-[#38BDF8] ">Book an Appointment</button>
                     </div>
                 </div>
